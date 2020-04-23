e1f02c03ccbea7330aced69f3b6e6d99bd3cd901
Duduxs <duduxss3@gmail.com>
2020-04-23 16:12:18 -0300

    Constraints implemented

---
+++ src/br/com/SellControll/application/gui/util/Constraints.java
@@ -0,0 +1,32 @@
+package br.com.SellControll.application.gui.util;
+
+import javafx.scene.control.TextField;
+
+public class Constraints {
+
+	public static void setTextFieldInteger(TextField txt) {
+		txt.textProperty().addListener((obs, oldValue, newValue) -> {
+			//Regular expression for any int
+			if (newValue != null && !newValue.matches("\\d*")) {
+				txt.setText(oldValue);
+			}
+		});
+	}
+
+	public static void setTextFieldMaxLength(TextField txt, int max) {
+		txt.textProperty().addListener((obs, oldValue, newValue) -> {
+			if (newValue != null && newValue.length() > max) {
+				txt.setText(oldValue);
+			}
+		});
+	}
+
+	public static void setTextFieldDouble(TextField txt) {
+		txt.textProperty().addListener((obs, oldValue, newValue) -> {
+			// Regular expression for decimal
+			if (newValue != null && !newValue.matches("\\d*([\\.]\\d*)?")) {
+				txt.setText(oldValue);
+			}
+		});
+	}
+}
