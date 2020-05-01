4c96fffad55054970e9ad59357e2e78180d6ea6a
Duduxs <duduxss3@gmail.com>
2020-05-01 02:28:36 -0300

    PoSRegistration FXML implemented

--- src/br/com/SellControl/gui/MainScreenControl.java
+++ src/br/com/SellControl/gui/MainScreenControl.java
@@ -9,8 +9,6 @@ import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 
 public class MainScreenControl implements Initializable {
 	// For identify the userLogged
@@ -60,7 +58,7 @@ public class MainScreenControl implements Initializable {
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		loadAndSetIcons();
+
 		// Set the username where i catch in EmployeeDAO logged in screen.
 		txtLoggedAs.setText(userLogged);
 
@@ -72,33 +70,4 @@ public class MainScreenControl implements Initializable {
 
 	}
 
-	// This method load and set icons!
-	public void loadAndSetIcons() {
-		// Loading the icons
-		Image imgClient = new Image(getClass().getResourceAsStream("/br/com/SellControl/img/ico/clientes.png"));
-		Image imgSetting = new Image(getClass().getResourceAsStream("/br/com/SellControl/img/ico/configuracoes.png"));
-		Image imgProvider = new Image(getClass().getResourceAsStream("/br/com/SellControl/img/ico/fornecedores.png"));
-		Image imgEmployee = new Image(getClass().getResourceAsStream("/br/com/SellControl/img/ico/funcionarios.png"));
-		Image imgProduct = new Image(getClass().getResourceAsStream("/br/com/SellControl/img/ico/produtos.png"));
-		//Image imgExit = new Image(getClass().getResourceAsStream("/br/com/SellControl/img/ico/sair.png"));
-		Image imgSell = new Image(getClass().getResourceAsStream("/br/com/SellControl/img/ico/vendas.png"));
-		// Saving their in ImageView
-		ImageView imgViewClient = new ImageView(imgClient);
-		ImageView imgViewSetting = new ImageView(imgSetting);
-		ImageView imgViewProvider = new ImageView(imgProvider);
-		ImageView imgViewEmployee = new ImageView(imgEmployee);
-		ImageView imgViewProduct = new ImageView(imgProduct);
-		//ImageView imgViewExit = new ImageView(imgExit);
-		ImageView imgViewSell = new ImageView(imgSell);
-
-		// Setting icons on the menu
-		menuClient.setGraphic(imgViewClient);
-		menuEmployee.setGraphic(imgViewEmployee);
-		menuProvider.setGraphic(imgViewProvider);
-		menuProduct.setGraphic(imgViewProduct);
-		menuSell.setGraphic(imgViewSell);
-		menuSettings.setGraphic(imgViewSetting);
-
-
-	}
 }
