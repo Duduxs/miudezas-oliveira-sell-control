adc6c73ed75d5db419088b03655f0103e9be9227
Duduxs <duduxss3@gmail.com>
2020-05-01 03:11:20 -0300

    Point of Sell 3.0 FXML

--- src/br/com/SellControl/application/Program.java
+++ src/br/com/SellControl/application/Program.java
@@ -16,7 +16,7 @@ public class Program extends Application {
 	public void start(Stage primaryStage) {
 		try {
 
-			Parent parent = FXMLLoader.load(getClass().getResource("/br/com/SellControl/gui/MainScreen.fxml"));
+			Parent parent = FXMLLoader.load(getClass().getResource("/br/com/SellControl/gui/PoS.fxml"));
 			Scene scene = new Scene(parent);
 			primaryStage.setScene(scene);
 			// Not resizable and size my screen to scene
